3b715ef184ef7890b92f4d8e3d193a8615757aa9
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-26 19:10:42 -0400

    oop

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const members = require("./Members")
+const moment = require("moment")
 
-const app = express(); 
+const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('<h1>Welcome!</h1>')
-})
+const logger = (req,res,next) =>{
+    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}: ${moment().format()}`)
+    next();
+}
 
+app.use(logger)
+
+app.get("/api/members", (req, res) => {
+    res.json(members);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
