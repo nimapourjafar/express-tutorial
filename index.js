744e61c121993925a4d23b2c5cc3c0138b238ef0
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-26 19:35:33 -0400

    api req

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members")
-const moment = require("moment")
+const logger = require("./middleware/logger")
 
 const app = express();
-
-const logger = (req,res,next) =>{
-    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}: ${moment().format()}`)
-    next();
-}
 
 app.use(logger)
 
 app.get("/api/members", (req, res) => {
     res.json(members);
 });
+
+app.get("/api/members/:id",(req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));   
+    }else{
+        res.status(400).json({msg:"Member not found"})
+    }
+})
 
 app.use(express.static(path.join(__dirname, "public")));
 
